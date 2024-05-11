5a768d0e8ce223447353a5d24a0abe362c4bfd18
adityaaaaaaaa1407 <adityasagavekar14@gmail.com>
2024-05-11 20:12:37 +0530

    setup the project and begin the work

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({ hello: "World" });
+});
+export const GET = handle(app);
+export const POST = handle(app);
---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import HeaderLogo from "./HeaderLogo";
+import NavigationPage from "./NavigationPage";
+
+const Header = () => {
+  return (
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36 ">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="w-full flex items-center justify-between mb-14">
+          <div className="flex items-center lg:gap-x-16">
+            <HeaderLogo />
+            <NavigationPage />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
